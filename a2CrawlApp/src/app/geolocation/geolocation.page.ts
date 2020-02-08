18ad7943f9cc2183b098f7e558c6683c44498c29
ZiadAbass <el16za@leeds.ac.uk>
2020-02-08 21:21:38 +0000

    showing the user on the map as an icon which can be pressed to reveal more info

--- a2CrawlApp/src/app/geolocation/geolocation.page.ts
+++ a2CrawlApp/src/app/geolocation/geolocation.page.ts
@@ -31,10 +31,37 @@ export class GeolocationPage implements OnInit, AfterViewInit {
         lat: this.latitude,
         lng: this.longitude
       };
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
       map.setCenter(pos);
+      // infoWindow.setPosition(pos);
+      const icon = {
+        url: 'assets/icon/u.png',
+        scaledSize: new google.maps.Size(50, 50),
+      };
+      const marker = new google.maps.Marker ({
+        position: pos,
+        map: map,
+        title: 'Hello Hello',
+        icon: icon
+      });
+      const contentString = '<div id="content">' +
+          '<div id="siteNotice">' +
+          '</div>' +
+          '<h1 id="firstHeading" class="firstHeading">Here you are</h1>' +
+          '<div id="bodyContent">' +
+          '<img src="assets/icon/u.png" width="200">' +
+          '<p>We can say all the things we want to over here, ' +
+          'maybe information about the location of the user </p>'
+          '<p>Attribution: Uluru, <a href="https://en.wikipedia.org</a> ' +
+          '(last visited June 22, 2019).</p>' +
+          '</div>' +
+          '</div>';
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 400
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
     }).catch((error) => {
       console.log('Error getting location', error);
     });
