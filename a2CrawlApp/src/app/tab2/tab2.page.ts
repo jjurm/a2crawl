c0d5b77c8e6c2d694d39215d38f752747bfd2384
ZiadAbass <el16za@leeds.ac.uk>
2020-02-08 19:45:41 +0000

    added basic map to tab2 that zooms in on specific long and lat

--- a2CrawlApp/src/app/tab2/tab2.page.ts
+++ a2CrawlApp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
+declare var google;
 
 @Component({
-  selector: 'app-tab2',
+  selector: 'app-home',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
 
-  constructor() {}
+export class Tab2Page implements OnInit, AfterContentInit {
+  map;
+  @ViewChild('mapElement', { static: true }) mapElement;
+  constructor() {
+  }
 
+  ngOnInit(): void {
+  }
+  ngAfterContentInit(): void {
+    this.map = new google.maps.Map(
+      this.mapElement.nativeElement,
+      {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+  }
 }
