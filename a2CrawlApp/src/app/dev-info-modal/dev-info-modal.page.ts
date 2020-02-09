a6b0c1ba977af3ba658722d997d37f4579e784f0
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2020-02-09 01:55:29 +0000

    Adding images. Fixing issues with package json

---
+++ a2CrawlApp/src/app/dev-info-modal/dev-info-modal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dev-info-modal',
+  templateUrl: './dev-info-modal.page.html',
+  styleUrls: ['./dev-info-modal.page.scss'],
+})
+export class DevInfoModalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
