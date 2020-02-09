7df0f488fb2aed045c890f6831a55914f9cf6d0e
williamlouth <williamlouth@outlook.com>
2020-02-09 02:39:46 +0000

    changes to app.py to deal with jsons

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/data/',method=['POST'])
+def data():
+    content = request.get_json()
+    return 'Hello data'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=5000)
